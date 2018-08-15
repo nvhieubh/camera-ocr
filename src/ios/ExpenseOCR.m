2548abc50a256143c97a4c7089a51b9c6ddd9837
Your Name <nvhieu.bh@gmail.com>
2018-08-15 17:06:49 +0700

    hieu commit

--- src/ios/ExpenseOCR.m
+++ src/ios/ExpenseOCR.m
@@ -60,7 +60,13 @@
         
         if (img != nil) {
             NSMutableDictionary *result = [NSMutableDictionary dictionaryWithDictionary:dic];
-            [result setObject:[UIImagePNGRepresentation(img) base64EncodedString] forKey:@"image"];
+            UIImage *imageCompress = [self resizeImage:img percent:1];
+            NSData* data = UIImageJPEGRepresentation(imageCompress, 0.4);
+            NSString *img64=[data base64EncodedString];
+            NSLog(@"img64 : %@\n", img64);
+            //long  lenImg =[img64 length];
+            //NSLog(@"Length of img64 :  %d\n", lenImg );
+            [result setObject:img64 forKey:@"image"];
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:result];
         } else {
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
@@ -72,6 +78,59 @@
         }];
         commandID = @"";
     }
+    
+}
+
+-(UIImage *)resizeImage:(UIImage *)image percent:(CGFloat)percent
+{
+    
+    NSInteger imageActualSize = UIImageJPEGRepresentation(image,1).length;
+    
+    NSLog(@"size of IMAGE before resizing: %@ ", [NSByteCountFormatter stringFromByteCount:imageActualSize countStyle:NSByteCountFormatterCountStyleFile]);
+    
+    float actualHeight = image.size.height;
+    float actualWidth = image.size.width;
+    float maxHeight = 400.0; // your custom  height
+    float maxWidth = 350; // your custom  width
+    float imgRatio = actualWidth/actualHeight;
+    float maxRatio = maxWidth/maxHeight;
+    float compressionQuality = percent;//50 percent compression
+    
+    if (actualHeight > maxHeight || actualWidth > maxWidth)
+    {
+        if(imgRatio < maxRatio)
+        {
+            //adjust width according to maxHeight
+            imgRatio = maxHeight / actualHeight;
+            actualWidth = imgRatio * actualWidth;
+            actualHeight = maxHeight;
+        }
+        else if(imgRatio > maxRatio)
+        {
+            //adjust height according to maxWidth
+            imgRatio = maxWidth / actualWidth;
+            actualHeight = imgRatio * actualHeight;
+            actualWidth = maxWidth;
+        }
+        else
+        {
+            actualHeight = maxHeight;
+            actualWidth = maxWidth;
+        }
+    }
+    
+    CGRect rect = CGRectMake(0.0, 0.0, actualWidth, actualHeight);
+    UIGraphicsBeginImageContext(rect.size);
+    [image drawInRect:rect];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    NSData *imageData = UIImageJPEGRepresentation(img, compressionQuality);
+    UIGraphicsEndImageContext();
+    
+    NSInteger imageReduceSize = imageData.length;
+    
+    NSLog(@"size of IMAGE after resizing: %@ ",[NSByteCountFormatter stringFromByteCount:imageReduceSize countStyle:NSByteCountFormatterCountStyleFile]);
+    
+    return [UIImage imageWithData:imageData];
     
 }
 
